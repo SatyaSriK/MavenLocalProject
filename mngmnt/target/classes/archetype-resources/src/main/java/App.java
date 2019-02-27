2bcd01c2b179120d515e6e8acb5ab8954c630c05
jenkins10@iiht.tech <jenkins10@iiht.tech>
2019-02-27 15:34:33 +0530

    First Commit from GIT
    Signed-off-by: jenkins10@iiht.tech <jenkins10@iiht.tech>

---
+++ mngmnt/target/classes/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.bell.mngmnt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
